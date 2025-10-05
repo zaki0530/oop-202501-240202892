c5ec996bf26992d664d164d78350d8b45492765e
zaki0530 <zki711824@gmail.com>
2025-10-05 14:40:46 +0700

    Add files via upload

---
+++ praktikum/week1-setup-hello-pos/src/hasil/HelloFunctional.java
@@ -0,0 +1,16 @@
+package main.java.com.upb.agripos;
+
+public class HelloFunctional {
+    public static void main(String[] args) {
+        String nim = "240202892";
+        // Perbaikan: Menggunakan huruf kapital pada awal nama (Kayla Putri Arsonisr)
+        String nama = "Abu zaki"; 
+
+        // Tampilan yang diminta
+        System.out.println("Hello World, I am " + nama + " -" + nim);
+
+        // Pesan penutup
+        System.out.println("Program Functional Selesai, Terimakasih");
+    }
+    
+}
---
+++ praktikum/week1-setup-hello-pos/src/hasil/HelloOop.java
@@ -0,0 +1,26 @@
+package main.java.com.upb.agripos;
+class Mahasiswa {
+    String nama;
+    String nim; // Menggunakan String untuk NIM
+
+    // Konstruktor dengan nama dan NIM
+    Mahasiswa(String n, String ni){
+        nama = n;
+        nim = ni;
+    }
+
+    // Metode sapa
+    void sapa(){
+        System.out.println("Halo, nama saya " + nama + " dengan NIM " + nim + ".");
+    }
+}
+public class HelloOop {
+    public static void main(String[] args) {
+        // Membuat objek Mahasiswa
+        Mahasiswa m = new Mahasiswa("Abu zaki", "240202892"); // Mengubah 'kayla' menjadi 'Kayla' agar lebih formal
+
+        // Memanggil metode sapa
+        m.sapa();
+    }
+    
+}
