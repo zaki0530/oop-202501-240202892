d9f6a6b9e75163631213ef83dca453aba522ec6c
abu zaki <zki711824@gmail.com>
2025-10-18 00:43:41 +0700

    laporan.md Produk,java CreditBy.java MainProduk.java

--- oop-202501-240202892/praktikum/week2-class-object/src/com/upb/agripos/util/CreditBy.java
+++
@@ -1,7 +0,0 @@
-package com.upb.agripos.util; 
-
-public class CreditBy {
-    public static void print(String nim, String nama) {
-        System.out.println("\ncredit by: " + nim + " - " + nama);
-    }
-}
