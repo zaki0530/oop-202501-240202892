b6a2184e06020755bca13b584756ed5d4d99c1e1
zaki0530 <zki711824@gmail.com>
2025-10-22 09:01:41 +0700

    Delete oop-202501-240202892 directory

--- oop-202501-240202892/praktikum/week2-class-object/src/main/java/com/upb/agripos/model/util/CreditBy.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.upb.agripos.model.util;
-
-public class CreditBy {
-    public static void print(String nim, String nama) {
-        System.out.println("\ncredit by: " + nim + " - " + nama);
-    }
-}
